3e1899e037ad366699ec6be7dbca844c92cfa783
O Anão Vascaino Bombado <d.brunetto@escola.pr.gov.br>
2026-02-20 11:11:40 +0000

    atividade dos times

--- exercicios/x02.py
+++ exercicios/x02.py
@@ -18,3 +18,16 @@ valores2 = [ 200.000, 320.000, 500.000]
 plt.bar(categorias2, valores2)
 plt.show()
 plt.savefig('./exercicios/x02.png')
+
+#3) Identifique na turma qual é o time de cada um e construa um gráfico de barras para mostrar a popularidade cada time.
+plt.clf()
+
+categorias3 = ['Vasco', 'grêmio', 'flamengo', 'internacional', 'palmeiras']
+valores3 = [1, 3, 2, 2, 1]
+cores = ["#030303", "#00B7FF", '#C52613', '#E5050F', '#006437']
+
+plt.bar(categorias3, valores3, color=cores)
+plt.xlabel('Timwa so campeonato')
+plt.ylabel('N* de torcedores')
+plt.show()
+plt.savefig('./exercicios/x03.png')

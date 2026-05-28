66a97c5708e8bbe630c20e336a34147263a261f3
O Anão Vascaino Bombado <d.brunetto@escola.pr.gov.br>
2026-05-28 12:46:07 +0000

    Aula 06 - snalise qualidativa usando histograma

--- Aulas/ayla06.py
+++ Aulas/ayla06.py
@@ -23,3 +23,25 @@ plt.ylabel("frequencia")
 
 plt.show()
 plt.savefig("aula06-qualitativo")
+
+plt.clf()
+
+notas = [
+    5, 6, 7, 8,
+    3, 6, 8, 3,
+    1, 2, 7, 0
+]
+
+serie = pd.Series(notas)
+frequencia = serie.value_counts()
+
+print(frequencia)
+
+serie.plot(kind="hist")
+
+plt.title("distribuição das notas")
+plt.xlabel("notas")
+plt.ylabel("frequencia")
+
+plt.show()
+plt.savefig("aula06-quantitatiivo")
